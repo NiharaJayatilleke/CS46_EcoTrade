b657dbbeb7ef0607b7733479ab4f10f18317d37e
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-10-30 04:12:01 +0530

    navigation bar

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -1,11 +1,13 @@
 <div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="<?php echo URLROOT?>/Users/login">Login</a>
-  <a href="<?php echo URLROOT?>/Users/register">Register</a>
-  <!-- <a href="#about">Logout</a> -->
+  <a class="active" >
+    <img src="../public/img/logo.png" alt="Logo" class="logo" width="80" height="30">
+  </a>
+  <a  href="#home">Home</a>
+  <a href="<?php echo URLROOT ?>/Users/login">Login</a>
+  <a href="<?php echo URLROOT ?>/Users/register">Signup</a>
+  <a  href="#postads">Post Ad</a>
+  <a  href="#wishlist">Saved Ads</a> 
   <input type="text" placeholder="Search in ecotrade">
- 
+  <a class="right" href="#all">All</a>
 </div>
-
-
 
