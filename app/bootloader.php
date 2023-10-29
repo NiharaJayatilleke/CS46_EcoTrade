006140a1b2d5c23c3b3cebb3ef213bcee9f8b73c
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-10-29 18:02:59 +0530

    adding php Mailer

--- app/bootloader.php
+++ app/bootloader.php
@@ -1,9 +1,11 @@
 <?php
-    //load configurations
+    //Load helpers
+    require_once 'helpers/URL_Helper.php';
+    require_once 'helpers/Session_Helper.php';
+    // Load configurations
     require_once 'config/config.php';
-    
-    //load libraries
+    // Load libraries
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';  
+    require_once 'libraries/Database.php';
 ?>
