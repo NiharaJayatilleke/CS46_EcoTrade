aba536a697bc1f50ac51a840e0cd2f7953a6054b
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2023-10-30 19:21:13 +0530

    Item ad form connected to the database

---
+++ app/views/item_ads/v_index.php
@@ -0,0 +1,9 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+    <!-- Top NAVIGATION -->
+    <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+
+    <h2>View your Ads here at the Secondhand Marketplace</h2>
+
+
+
+<?php require APPROOT.'/views/inc/footer.php'; ?>
