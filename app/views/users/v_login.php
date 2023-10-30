36c891652a457fb3a7f529de3b11767477e6bcdc
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2023-10-30 14:12:54 +0530

    Implemented registration of Moderators by admin and Admin/Moderator Login

--- app/views/users/v_login.php
+++ app/views/users/v_login.php
@@ -7,7 +7,7 @@
         <center><h1>Login</h1></center>
         <p><b>Welcome to EcoTrade! Please Login to continue.</b></p>
         </div>
-        <form action="" method="POST">
+        <form action="<form action="<?php echo URLROOT?>/Users/login" method="POST">
 
             <!-- email -->
             <div class="form-input-title">Email</div>
