6780bbc0e87a509f624e5b5f6eea499561febed2
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-10-30 12:58:29 +0530

    topnavbar

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -1,13 +1,21 @@
 <div class="topnav">
-  <a class="active" >
-    <img src="../public/img/logo.png" alt="Logo" class="logo" width="80" height="30">
-  </a>
-  <a  href="#home">Home</a>
-  <a href="<?php echo URLROOT ?>/Users/login">Login</a>
-  <a href="<?php echo URLROOT ?>/Users/register">Signup</a>
-  <a  href="#postads">Post Ad</a>
-  <a  href="#wishlist">Saved Ads</a> 
-  <input type="text" placeholder="Search in ecotrade">
-  <a class="right" href="#all">All</a>
+  <div class="items">
+    <div class="item logo">
+    <a href="">
+      <img src="../public/img/logo.png" alt="Logo" class="logo" width="80" height="30">
+    </a>
+    </div>
+    <div class="item search" style="display: flex">
+      <input type="text" placeholder="Search in ecotrade">
+      <img src="../public/img/search.png" alt="menu" class="" width="20px" height="20px" style="padding-top: 10px" >
+    </div>
+    <div class="links">
+      <div class="item"><a href="">Home</a></div>
+      <div class="item"><a href="<?php echo URLROOT ?>/Users/login">Login</a></div>
+      <div class="item"><a href="<?php echo URLROOT ?>/Users/register">SignUp</a></div>
+      <div class="item"><a href="<?php echo URLROOT ?>/ItemAds/itemAd">Post ad</a></div>
+      <div class="item"><a href="#wishlist">Saved Ads</a></div>
+      <div class="item" onclick="toggle()"><img src="../public/img/menu.png" alt="menu" class="menu" width="80" height="30"></div>
+    </div>
+  </div>
 </div>
-
