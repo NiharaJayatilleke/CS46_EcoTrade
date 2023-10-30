d8800b4f3f5829d27f3901aef9896e946df65c9b
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-10-31 00:25:20 +0530

    v_index page corrected

--- app/views/pages/v_index.php
+++ app/views/pages/v_index.php
@@ -1,11 +1,11 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
     <!-- Top NAVIGATION -->
     <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
-
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_index_styles.css">
     <h1 style="margin-top:60px; font-size:15px;color:white;margin-left:20px;">Welcome <?php echo isset( $_SESSION['user_name']) ? $_SESSION['user_name'] : ''; ?>!</h1>
     <div class="item search" style="display: flex">
       <input type="text" placeholder="Search in ecotrade">
-      <img src="../public/img/search.png" alt="search" class="searchicon" width="20px" height="20px"  >
+      <img src="<?php echo URLROOT; ?>/public/img/search.png" alt="search" class="searchicon" width="20px" height="20px">
     </div>
     <div class="bg-img">
         <img src="../public/img/home.jpg" alt="">
