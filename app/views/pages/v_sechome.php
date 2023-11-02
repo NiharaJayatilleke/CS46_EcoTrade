b205b7574a45382a95c7250ecaa5aa3971807f28
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2023-11-02 20:11:22 +0530

    stylingfinal

---
+++ app/views/pages/v_sechome.php
@@ -0,0 +1,74 @@
+
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+<!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_wishlist_styles.css"> -->
+
+
+        <div class="main-content">
+            <div class="photo-left">
+                <img src="/public/img/sechome/girl1.png" alt="Photo 1">
+            </div>
+            <div class="content-center">
+                <div class="welcome-text">
+                    <h1>Welcome to EcoTrade</h1>
+                    <p class="p1">The new marketplace for modern Sri Lankans</p>
+                    <button class="b1">All of Sri Lanka</button>
+                </div>
+                <div class="search-bar">
+                    <input type="text" placeholder="Search...">
+                    <button>Search</button>
+                </div>
+                <div class="find-ads">
+                    <p class="p1">Find the best deals on Vehicles, Property, Electronics and more. Choose from a wide range of high quality ADs from
+                        trusted sellers.</p>
+                    <button>Explore Ads</button>
+                </div>
+            </div>
+            <div class="photo-right">
+                <img src="/public/img/sechome/girl1.png" alt="Photo 2">
+            </div>
+        </div>
+
+        <div class="featured-categories">
+            <h2 class="head1">Featured Category</h2>
+            <p class="p2">Browse through some of our most popular categories</p>
+        
+            <div class="category-photos">
+                <div class="photo" onclick="location.href='navBar.html';">
+                    <img src="/public/img/sechome/p1.png" alt="Category 1">
+                    <p>Motors</p>
+                </div>
+                <div class="photo">
+                    <img src="/public/img/sechome/p2.png" alt="Category 2">
+                    <p>Mobiles And Electronics</p>
+                </div>
+                <div class="photo">
+                    <img src="/public/img/sechome/p3.png" alt="Category 3">
+                    <p>Others</p>
+                </div>
+            </div>
+            <div class="category-photos">
+                <div class="photo">
+                    <img src="/public/img/sechome/p4.png" alt="Category 1">
+                    <p>Hobby, Sport & Kids</p>
+                </div>
+                <div class="photo">
+                    <img src="/public/img/sechome/p5.png" alt="Category 2">
+                    <p>Fashion & Beauty</p>
+                </div>
+                <div class="photo">
+                    <img src="/public/img/sechome/p6.png" alt="Category 3">
+                    <p>Home and Garden</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    
+
+
+
+
+
+
