58a012d05170da3e6f708580213dcaa430068ee3
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-11-06 23:46:15 +0530

    search bar

--- app/views/pages/v_index.php
+++ app/views/pages/v_index.php
@@ -1,4 +1,5 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
+
 <!-- Top NAVIGATION -->
 <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
 
@@ -6,9 +7,16 @@
 
 <!-- <h1 style="margin-top:25px; font-size:15px; color:; margin-left:40px;">Welcome <?php echo isset($_SESSION['user_name']) ? $_SESSION['user_name'] : ''; ?>!</h1> -->
 
-<div class="item search" style="display: flex">
+<!-- <div class="item search" style="display: flex">
   <input type="text" class = "placeholder-text" placeholder="Search in EcoTrade">
   <img src="<?php echo URLROOT; ?>/public/img/search.png" alt="search" class="searchicon" width="20px" height="20px">
+</div> -->
+
+ <div class="search-container" >
+  <input type="text" class="search-input" placeholder="Search in EcoTrade">
+  <button class="search-button">
+    <img src="<?php echo URLROOT; ?>/public/img/search.png" alt="search" class="search-icon">
+  </button>  
 </div>
 <div class="bg-img">
     <img src="../public/img/home.png" alt="">
