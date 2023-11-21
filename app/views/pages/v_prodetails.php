aca5f26ba75893b4c0a5554e6a49bc32153bfb78
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2023-11-21 16:41:54 +0530

    productdetails_1

---
+++ app/views/pages/v_prodetails.php
@@ -0,0 +1,90 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_prodetails.css">
+
+
+<div class="main-container1">
+    <div class="main2"></div>
+        <h1>LAND ROVER</h1>
+        <p class="p1">Posted on 5/12/2023 6:53:14 AM, COLOMBO, AKARAWITA</p>
+        <div class="container2">
+        <div class="left-container">
+        
+            <div class="big-photo">
+                <img id="bigImage" src="/pics/productDetails1.png" alt="Small Image 1">
+            </div>
+            <div class="small-images">
+                <!-- <img id="s1" src="productDetails1.png" alt="Small Image 1" onclick="displayBigImage('productDetails1.png')"> -->
+                <img id="s1" src="/pics/productDetails1.png" alt="Small Image 1" onclick="displayBigImage('productDetails1.png')">
+
+                <img id="s2" src="/pics/productDetails2.jpeg" alt="Small Image 2" onclick="displayBigImage('/pics/productDetails2.jpeg')">
+                <!-- Add more small images as needed -->
+            </div>
+            <div class="desMain">
+                <div class="heading">
+                    <h1>Rs 7,600,000</h1>
+                    <p>Negotiable</p>
+                </div>
+
+                <div class="description">
+                    <div class="desHead">
+                        <h3> Description</h1>
+                    </div>
+                    <div class="desP">
+                        <p>YOM 1971 YOR 1979<br>300 TDI Engine R380 Gearbox</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="bottom">
+                <button class="b1">
+                    <img src="/pics/promote.png" alt="promote">
+                    <p>Promote This Ad</p>
+                </button>
+                <button class="b1">
+                    <img src="/pics/report.png" alt="report">
+                    <p>Report This Ad</p>
+                </button>
+                <button class="b1">
+                    <img src="/pics/save.png" alt="report">
+                    <p>Save this Ad</p>
+                </button>
+            </div>
+        
+    </div>
+
+    <div class="right-container">
+        <div class="b1">
+            <img class="i1" src="/pics/profile.png" alt="profile">
+            <div class="b1_2">
+                <p>Mahesh</p>
+            </div>
+        </div>
+        <div class="b1">
+            <img src="/pics/phone1.png" alt="profile">
+            <div class="b2">
+            <h4>076XXXXXXX</h2>
+            <p>Click to show phone number</p>
+        </div>
+        </div>
+        <div class="b1">
+            <img src="/pics/phone2.png" alt="profile">
+            <div class="b2">
+            <h4>076XXXXXXX</h2>
+            <p>Click to show phone number</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+    <script src="productDetails.js"></script>
+    </body>
+    </html>
+    
+
+
+
