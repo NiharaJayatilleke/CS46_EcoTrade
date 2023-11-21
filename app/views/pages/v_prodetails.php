528d376b8befc97da36066de38ac5b1294de313f
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2023-11-21 17:05:04 +0530

    productdetails_completed

--- app/views/pages/v_prodetails.php
+++ app/views/pages/v_prodetails.php
@@ -12,13 +12,13 @@
         <div class="left-container">
         
             <div class="big-photo">
-                <img id="bigImage" src="/pics/productDetails1.png" alt="Small Image 1">
+                <img id="bigImage" src="../public/img/prodetails/productDetails1.png" alt="Small Image 1">
             </div>
             <div class="small-images">
                 <!-- <img id="s1" src="productDetails1.png" alt="Small Image 1" onclick="displayBigImage('productDetails1.png')"> -->
-                <img id="s1" src="/pics/productDetails1.png" alt="Small Image 1" onclick="displayBigImage('productDetails1.png')">
+                <img id="s1" src="../public/img/prodetails/productDetails1.png" alt="Small Image 1" onclick="displayBigImage('productDetails1.png')">
 
-                <img id="s2" src="/pics/productDetails2.jpeg" alt="Small Image 2" onclick="displayBigImage('/pics/productDetails2.jpeg')">
+                <img id="s2" src="../public/img/prodetails/productDetails2.jpeg" alt="Small Image 2" onclick="displayBigImage('/pics/productDetails2.jpeg')">
                 <!-- Add more small images as needed -->
             </div>
             <div class="desMain">
@@ -41,15 +41,15 @@
 
             <div class="bottom">
                 <button class="b1">
-                    <img src="/pics/promote.png" alt="promote">
+                    <img src="../public/img/prodetails/promote.png" alt="promote">
                     <p>Promote This Ad</p>
                 </button>
                 <button class="b1">
-                    <img src="/pics/report.png" alt="report">
+                    <img src="../public/img/prodetails/report.png" alt="report">
                     <p>Report This Ad</p>
                 </button>
                 <button class="b1">
-                    <img src="/pics/save.png" alt="report">
+                    <img src="../public/img/prodetails/save.png" alt="report">
                     <p>Save this Ad</p>
                 </button>
             </div>
@@ -58,20 +58,20 @@
 
     <div class="right-container">
         <div class="b1">
-            <img class="i1" src="/pics/profile.png" alt="profile">
+            <img class="i1" src="../public/img/prodetails/profile.png" alt="profile">
             <div class="b1_2">
                 <p>Mahesh</p>
             </div>
         </div>
         <div class="b1">
-            <img src="/pics/phone1.png" alt="profile">
+            <img src="../public/img/prodetails/phone1.png" alt="profile">
             <div class="b2">
             <h4>076XXXXXXX</h2>
             <p>Click to show phone number</p>
         </div>
         </div>
         <div class="b1">
-            <img src="/pics/phone2.png" alt="profile">
+            <img src="../public/img/prodetails/phone2.png" alt="profile">
             <div class="b2">
             <h4>076XXXXXXX</h2>
             <p>Click to show phone number</p>
