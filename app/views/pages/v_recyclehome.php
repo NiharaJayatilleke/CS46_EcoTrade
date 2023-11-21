8957c1d8cda9327f0fb7501d0505c078144aa78a
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2023-11-21 16:33:40 +0530

    recyclehome

---
+++ app/views/pages/v_recyclehome.php
@@ -0,0 +1,31 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_recyclehome.css">
+
+<div class="big-photo-container">
+    <img class="big-photo" src="../public/img/rec/recycleMarkertplace.png" alt="Big Photo">
+</div>
+<div class="d2">
+   <div class="d2_1">
+    <img src="../public/img/rec/pic1.png" alt="sell">
+   </div>
+   <div class="d3">
+    <div class="d3_1">
+        <img src="../public/img/rec/pic2.png" alt="olastic">
+    </div>
+    <div class="d3_2">
+        <img src="../public/img/rec/pic3.png" alt="collector">
+        <a href="<?php echo URLROOT; ?>/Users/register">
+    <button class="sign-up-button">Sign Up Here</button>
+</a>
+    
+    </div>
+   </div>
+</div>
+
+    </body>
+</html>
+    
+
+
