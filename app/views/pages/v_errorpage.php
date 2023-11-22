1f36d65bcb417b6b68d9c21c505463ed62d0cf8b
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2023-11-22 16:33:09 +0530

    errorpage_php

---
+++ app/views/pages/v_errorpage.php
@@ -0,0 +1,19 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_errorpage.css">
+
+
+<div class="div">
+  <img
+    loading="lazy"
+    src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca6faaeb-2935-46ac-ad67-0b1c90999935?"
+    class="img"
+  />
+  <div class="content">
+      <div class="text-wrapper">404, Page not founds</div>
+      <p class="div">
+        Something went wrong. It’s look that your requested could not be found. It’s look like the link is broken or the
+        page is removed.
+      </p>
+      <button class="button-go-back">Go Back</button>
