bbeeb0f4dae76eb9b1c59d314ab5ccf7d76dc4ec
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2023-12-28 01:06:17 +0530

    item_ad_styling and popup boxes

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,8 +6,6 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
         <title><?php echo SITENAME; ?></title>
-
-               
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     </head>
     <body>
