060ed7dbdf816be6077b3fec1a8f964bdb3b2a2a
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2024-01-03 19:15:57 +0530

    redirection based on item type

---
+++ app/views/item_ads/v_itemtype.php
@@ -0,0 +1,17 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+    <!-- Top NAVIGATION -->
+    <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+
+    <form action="<?php echo URLROOT?>/ItemAds/itemType" method="POST" enctype="multipart/form-data">
+
+        <div class="type-container">
+            <!-- item_type -->
+            <div class="input-type-title">Please select the item type</div>
+            <input type="radio" id="secondhand" name="item_type" value="secondhand">
+            <label for="secondhand">Secondhand Item</label><br>
+            <input type="radio" id="recycle" name="item_type" value="recycle">
+            <label for="recycle">Recyclable item</label><br>
+
+            <button class="continue" type="submit"><i class="fas fa-arrow-right"></i></button>        </div>
+
+    <?php require APPROOT.'/views/inc/footer.php'; ?>
