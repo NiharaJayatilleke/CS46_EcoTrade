311828554ea95e58a008f3ea0d46619f2fa17127
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-01-07 17:32:38 +0530

    login done

---
+++ app/views/users/forgot_password.php
@@ -0,0 +1,29 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/inc/components/topnavbar.php'; ?>
+
+<div class="hero2">
+    <div class="form-box2">
+        <div class="form-container">
+            <form action="<?php echo URLROOT; ?>/forgotPassword/sendEmail" method="POST">
+                <h2 style="text-align: center; font-size: 24px; color: #333; margin-bottom: 40px;">Reset Password</h2>
+
+                <p style="margin: 5px; padding-left: 30px; padding-right: 3px;">Please enter the email address associated with your registered account.</p><br>
+
+                <input type="email" name="email" placeholder="Email" id="email" class="input-field email" value="<?php echo $data['email']; ?>">
+                <span class="form-invalid"><?php echo $data['email_err']; ?></span>
+
+                <input type="submit" value="Receive Email" class="submit-btn"> <br>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require APPROOT.'/views/inc/footer.php'; ?>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="path/to/your/animations.js"></script>
+<script src="/public/js/input_validation.js"></script>
+
+</body>
+</html>
--- app/views/users/signup.php
+++ app/views/users/signup.php
@@ -26,43 +26,47 @@
 
                     <div class="input-box">
                         <span class="icon"><i class='bx bxs-contact' ></i></span>
-                        <input type="name" id="name" class="name" value="<?php echo $data['name']; ?>" required>
+                        <input type="name" name = "username" id="username" class="username" value="<?php echo $data['username']; ?>" required>
                         <label>Name</label>
 
                     </div>
 
                     <div class="input-box">
                         <span class="icon"><i class='bx bxs-envelope'></i></span>
-                        <input type="email" id="email" class="email" value="<?php echo $data['email']; ?>" required>
+                        <input type="email" name = "email" id="email" class="email" value="<?php echo $data['email']; ?>" required>
                         <label>Email</label>
-                        <!-- <span class="form-invalid"><?php echo $data['username_err']; ?></span> -->
+                        <!-- <span class="form-invalid"><?php echo $data['email_err']; ?></span> -->
 
                     </div>
 
                     <div class="input-box">
-                        <span class="icon"><i class='bx bxs-phone-call'></i></span>
-                        <input type="number" id="number" class="number" value="<?php echo $data['number']; ?>" required >
+                        <span class="icon"><i class='bx bxs-phone'></i></span>
+                        <input type="tel" name = "number" id="number" class="number" value="<?php echo $data['number']; ?>" required>
                         <label>Contact Number</label>
-                        <!-- <span class="form-invalid"><?php echo $data['number_err']; ?></span> -->
+                        <span class="form-invalid"><?php echo $data['number_err']; ?></span>
                     </div>
+
 
                     <div class="input-box">
                         <span class="icon"><i class='bx bxs-lock-alt' ></i></span>
-                        <input type="password" id="password" value="<?php echo $data['password']; ?>" required >
+                        <input type="password" name = "password" id="password" value="<?php echo $data['password']; ?>" required >
                         <label>Password</label>
-                        <!-- <span class="form-invalid"><?php echo $data['password_err']; ?></span> -->
+                        <span class="form-invalid"><?php echo $data['password_err']; ?></span>
                     </div>
 
                     <div class="input-box">
                         <span class="icon"><i class='bx bxs-lock-alt' ></i></span>
-                        <input type="password" id="password" value="<?php echo $data['password']; ?>" required >
+                        <input type="password" name = "confirm_password" id="password" value="<?php echo $data['password']; ?>" required >
                         <label>Confirm Password</label>
-                        <!-- <span class="form-invalid"><?php echo $data['confirm_password_err']; ?></span> -->
+                        <span class="form-invalid"><?php echo $data['confirm_password_err']; ?></span>
                     </div>
 
                     <div class="terms-forgot">
                         <label><input type="checkbox"name="agree" id="agree" value="yes"/> I agree to the
                         <a href="<?php echo URLROOT; ?>/Users/terms" title="term of services">terms & conditions</a>
+                    </div>
+                    <div>
+                        <span class="form-invalid"><?php echo $data['agree_err']; ?></span>
                     </div>
 
                     <button type="submit" class = "register-btn">Register</button>
