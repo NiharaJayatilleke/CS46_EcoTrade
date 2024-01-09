9acd0ad93334a97dba381a1980fc17279640f884
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-01-09 12:22:32 +0530

    wishlist4

--- app/controllers/Wishlist.php
+++ app/controllers/Wishlist.php
@@ -1,13 +1,38 @@
 <?php
     class Wishlist extends Controller{
         public function __construct(){
-            $this->pagesModel =$this->model('M_Pages');
+            $this->wishlistModel =$this->model('M_Wishlist');
+           
+            
         }
 
         public function index(){
             $data = [];
             $this->view('pages/v_wishlist',$data);
 
+        }
+        public function addToWishlist($id){
+
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+                $data = [
+                    'ad_id' => $id,
+                    'user_id' => $_SESSION['user_id'],
+                ];
+
+                try {
+                    if ($this->wishlistModel->addItem($data)) {
+                        flash('wishlist_success','Item added successfully');
+
+                    } else {
+                        die('Something went wrong with adding the offer');
+                    }
+
+                } catch (Exception $e) {
+                    die('Error: ' . $e->getMessage());
+                }
+
+            }
         }
 
         
