81cdda97db1ffe7a878c0aedd89656ee53d678db
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-01-24 22:10:24 +0530

    sechome1

--- app/views/pages/v_recyclehome.php
+++ app/views/pages/v_recyclehome.php
@@ -2,7 +2,7 @@
 <!-- Top NAVIGATION -->
 <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_recyclehome.css">
-
+<!-- 
 <div class="big-photo-container">
     <img class="big-photo1" src="../public/img/rec/recycleMarkertplace.png" alt="Big Photo">
 </div>
@@ -23,7 +23,7 @@
     </div>
    </div>
 
-</div> -->
+</div> --> 
 
 <div class="slider">
         <!-- list Items -->
@@ -124,35 +124,35 @@
       </h3>
       <p class="card-hover__text">Turn your plastic waste into money today! Click "Sell Now" and make a difference. </p>
       <a href="#" class="card-hover__link">
-        <span>Learn How</span>
+        <span>Sell Now</span>
         <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
         </svg>        
       </a>
     </div>
     <div class="card-hover__extra">
-      <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
+      <h4>Join <span>now</span> and post your <span>Ad</span> for free!</h4>
     </div>
-    <img src="https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=60" alt="">
+    <img src="../public/img/Recyclehome/image/portrait-cute-young-girl-recycling.jpg" alt="">
   </div>
 
   <div class="card-hover">
     <div class="card-hover__content">
       <h3 class="card-hover__title">
-        Make your <span>choice</span> right now!
+        Become a <span>Collector</span> right now!
       </h3>
-      <p class="card-hover__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
+      <p class="card-hover__text">Become a recycle item collector now to save and earn. Click "Join Now" and make a positive impact while growing your income</p>
       <a href="#" class="card-hover__link">
-        <span>Learn How</span>
+        <span>Join Now</span>
         <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
         </svg>        
       </a>
     </div>
     <div class="card-hover__extra">
-      <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
+      <h4>Join <span>now</span>  and post your <span>ad</span> for free!</h4>
     </div>
-    <img src="https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=60" alt="">
+    <img src="../public/img/Recyclehome/image/recycling-waste-is-very-necessary-environment.jpg" alt="">
   </div>
 </div>
     <!-- <div class="cards">
--- app/views/pages/v_sechome.php
+++ app/views/pages/v_sechome.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/v_sechome.css">
 
 
-        <div class="main-content">
+        <!-- <div class="main-content">
             <div class="photo-left">
                 <img src="../public/img/sechome/girl1.png" alt="Photo 1">
             </div>
@@ -62,7 +62,337 @@
                     <p>Home and Garden</p>
                 </div>
             </div>
+        </div> -->
+
+
+
+        <div class="row">
+          <div class="col-2">
+            <h1>Give Your Workout<br />A New Style!</h1>
+            <p>
+              Success isn't always about greatness. It's about consistency.
+              Consistent<br />
+              hard work gains success. Greatness will come.
+            </p>
+            <a href="" class="btn">Explore Now &#8594; </a>
+          </div>
+          <div class="col-2">
+            <img src="../public/img/sechome/images/15-removebg-preview.png" alt="Two men being active" />
+          </div>
         </div>
+      </div>
+    </div>
+
+    <!-- Featured Categories -->
+
+    <div class="categories">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-3">
+            <img
+              src="../public/img/sechome/images/category-1.jpg"
+              alt="a man's legs with bright orange shoes"
+            />
+          </div>
+          <div class="col-3">
+            <img
+              src="../public/img/sechome/images/category-2.jpg"
+              alt="black and white addidas shoes"
+            />
+          </div>
+          <div class="col-3">
+            <img
+              src="../public/img/sechome/images/category-3.jpg"
+              alt="a woman in an addidas hoodie and white watch"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Featured Products -->
+
+    <div class="small-container">
+      <h2 class="title">Featured Products</h2>
+      <div class="row">
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-1.jpg" alt="Red T-Shirt" />
+          <h4>Red Printed T-Shirt</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-o"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$50.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-2.jpg" alt="Black Shoes" />
+          <h4>Black HRX Shoes</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$197.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-3.jpg" alt="Grey Sweatpants" />
+          <h4>Gray Benetton Sweatpants</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <p>$75.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-6.jpg" alt="Black T-Shirt" />
+          <h4>Black Printed T-Shirt</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <p>$50.00</p>
+        </div>
+      </div>
+
+      <h2 class="title">Latest Products</h2>
+      <div class="row">
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-7.jpg" alt="Tri-Color Socks" />
+          <h4>HRX 3pk Socks</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-o"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$15.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-2.jpg" alt="Black Shoes" />
+          <h4>Black HRX Shoes</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$197.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-8.jpg" alt="Black Watch" />
+          <h4>Fossil Watch</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <p>$145.00</p>
+        </div>
+        <div class="col-4">
+          <img src="../public/img/sechome/images/product-11.jpg" alt="Grey Shoes" />
+          <h4>Gray Shoes</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <p>$245.00</p>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <img src="../public/img/sechome/images/product-10.jpg" alt="Black and Red Shoes" />
+            <h4>Black Shoes</h4>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <i class="fa fa-star-o"></i>
+            </div>
+            <p>$15.00</p>
+          </div>
+          <div class="col-4">
+            <img src="../public/img/sechome/images/product-9.jpg" alt="Black Watch" />
+            <h4>Fossil Watch</h4>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-o"></i>
+            </div>
+            <p>$197.00</p>
+          </div>
+          <div class="col-4">
+            <img src="../public/img/sechome/images/product-12.jpg" alt="Black Pants" />
+            <h4>Magic Pants</h4>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </div>
+            <p>$145.00</p>
+          </div>
+          <div class="col-4">
+            <img src="../public/img/sechome/images/product-4.jpg" alt="Blue Shirt" />
+            <h4>Shirty Shirt</h4>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </div>
+            <p>$245.00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Offer -->
+
+    <div class="offer">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-2">
+            <img
+              src="../public/img/sechome/images/exclusive.png"
+              class="offer-img"
+              alt="orange watch"
+            />
+          </div>
+          <div class="col-2">
+            <p>Exclusively Available on RedStore</p>
+            <h1>Smart Band 4</h1>
+            <small
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+              sapiente accusamus! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit ham.</small
+            >
+            <a href="" class="btn">Buy Now &#8594;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Testimonials -->
+
+    <div class="testimonial">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+              nesciunt doloremque maxime quidem necessitatibus sed repellat ea
+              officia quibusdam! Sunt.
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </div>
+            <img src="../public/img/sechome/images/user-1.png" alt="human face" />
+            <h3>Grace Cho</h3>
+          </div>
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+              nesciunt doloremque maxime quidem necessitatibus sed repellat ea
+              officia quibusdam! Sunt.
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </div>
+            <img src="../public/img/sechome/images/user-2.png" alt="human face" />
+            <h3>Frank Garett</h3>
+          </div>
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+              nesciunt doloremque maxime quidem necessitatibus sed repellat ea
+              officia quibusdam! Sunt.
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </div>
+            <img src="../public/img/sechome/images/user-3.png" alt="human face" />
+            <h3>Charolette Rue</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Brands -->
+
+    <div class="brands">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-5">
+            <img src="../public/img/sechome/images/logo-godrej.png" alt="godrej logo" />
+          </div>
+          <div class="col-5">
+            <img src="../public/img/sechome/images/logo-coca-cola.png" alt="coke logo" />
+          </div>
+          <div class="col-5">
+            <img src="../public/img/sechome/images/logo-oppo.png" alt="oppo logo" />
+          </div>
+          <div class="col-5">
+            <img src="../public/img/sechome/images/logo-paypal.png" alt="paypal logo" />
+          </div>
+          <div class="col-5">
+            <img src="../public/img/sechome/images/logo-philips.png" alt="philips logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+
+
+    <!-- JS for Menu Toggle-->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+
+      MenuItems.style.maxHeight = "0px";
+
+      function menutoggle() {
+        if (MenuItems.style.maxHeight == "0px") {
+          MenuItems.style.maxHeight = "200px";
+        } else {
+          MenuItems.style.maxHeight = "0px";
+        }
+      }
+    </script>
     </body>
     </html>
     
