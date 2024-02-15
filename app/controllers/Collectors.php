9bd9b64e4592942ccd5cf5567962eead5ce9c192
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-02-15 13:16:20 +0530

    link corrected

--- app/controllers/Collectors.php
+++ app/controllers/Collectors.php
@@ -1,18 +1,19 @@
 <?php
     class Collectors extends Controller{
         public function __construct(){
+            // $this->pagesModel =$this->model('M_Collectors');
             $this->pagesModel =$this->model('M_Pages');
-            // $this->collectorModel =$this->model('M_Offers');
+
         }
 
         public function index(){
             $data = [];
-            $this->view('pages/users/collectors/index',$data);
+            $this->view('users/collectors/index',$data);
         }
 
         public function about(){
             $data = [];
-            $this->view('pages/users/collectors/about',$data);
+            $this->view('users/collectors/about',$data);
         }
     }
 ?>
