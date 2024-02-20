1467fd341ac5c6aa9983501593d8f4a3131539f3
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-02-20 14:09:03 +0530

    adpayment

--- app/views/pages/v_addpayment.php
+++ app/views/pages/v_addpayment.php
@@ -6,44 +6,241 @@
 
 
 
-
-<div class="w3-row-padding">
-
-<div class="w3-third w3-margin-bottom">
-  
-  <ul class="w3-ul w3-border w3-center w3-hover-shadow">
-    <li class="w3-green w3-xlarge w3-padding-32">Pay and Highlight Your Ad</li>
-    <li class="w3-padding-16"><b>Bring it to TOP</li>
-    <li class="w3-padding-16"><b>Make more REACH</li>
-    <li class="w3-padding-16"><b>Get more CUSTOMERS</li>
-    <li class="w3-padding-16"><b>At small COST</li>
-    
-    <li class="w3-padding-16">
-        <div class="AddPayment_d1">
-            <div class="AddPayment_d2">
-                <h2 class="w3-wide">$25</h2>
-                <span class="w3-opacity">per month</span>
+<div class="main">
+<div class="divv">
+    <div class="divv-2">
+      <div class="divv-3">Make your ad stand out!</div>
+      <div class="divv-4">
+        Get up to 10 times more responses by applying Ad Promotions.
+      </div>
+      <div class="divv-5">
+        <div class="divv-6">Select one or more options</div>
+        <div class="divv-7">(Optional).</div>
+      </div>
+      <div class="divv-8">
+      <img
+                  loading="lazy"
+                  src="../public/img/payment/images/new.png"
+                  class="img"
+                />
+        <div class="divv-9">Now you can schedule your Bump Up and Top Ad</div>
+      </div>
+      <div class="divv-300">
+      <div class="divv-10">Recommended</div>
+      <div class="divv-11">
+        <div class="divv-12">
+          <div class="column">
+            <div class="divv-13">
+              <div class="divv-14">
+                <img
+                  loading="lazy"
+                  src="../public/img/payment/images/new.png"
+                  class="img-2"
+                />
+                <div class="divv-15">
+                  <img src="../public/img/payment/images/rocket.png" alt="rocket">
+                  />
+                  <div class="divv-16"></div>
+                </div>
+              </div>
+              <div class="divv-17">
+                <div class="divv-18">Bump Up</div>
+                <div class="divv-19">
+                  Get a fresh start every day and get up to 10 times more
+                  responses!
+                </div>
+              </div>
             </div>
+          </div>
+          <div class="column-2">
+            <div class="divv-20">
+              <div class="divv-21">7 days</div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2efc887e61649c3500b038466a90812f0da0e569c2199eede2f5a673af3dd4ef?"
+                class="img-4"
+              />
+            </div>
+          </div>
         </div>
-    </li>
-    
-    <li class="w3-light-grey w3-padding-24">
-    <button class="w3-button w3-green w3-padding-large" onclick="redirectToWebsite()">Pay Now</button>
-     
-    </li>
-  </ul>
+      </div>
+      <div class="divv-22">
+        <div class="divv-23">
+          <div class="divv-24">
+            <div class="divv-25">
+              <img
+                loading="lazy"
+                srcset="../public/img/payment/images/svg.png"
+                class="img-5"
+              />
+              <div class="divv-26">
+                <div class="divv-27">Top Ad</div>
+                <div class="divv-28">
+                  Get up to 5 times more views by displaying your ad at the top!
+                </div>
+              </div>
+            </div>
+            <div class="divv-29">
+              <div class="divv-30">From Rs 1,100</div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/1706ae8f17adcaf7f3403231ebb015c8462dfa93fb8f06f41d3f691158239c4b?"
+                class="img-6"
+              />
+            </div>
+          </div>
+          <div class="divv-31"></div>
+          <div class="divv-32">
+            <div class="divv-33">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc75bb169724f5a2c3ad26a3e4ea0efe0089d85605b2b9ab79f04a5b4462b9fe?"
+                class="img-7"
+              />
+              URGENT
+            </div>
+            <div class="divv-35">Bundle</div>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/363947920ee0afa51e407e9192e3891965ea684742c0de95f1a0652b532491c0?"
+              class="img-8"
+            />
+            <div class="divv-36">Sell faster for less!</div>
+          </div>
+          <div class="divv-37">
+            <div class="divv-38">
+              <div class="divv-39">Ad will be promoted as Urgent for 3 days</div>
+              <div class="divv-40">
+                Ad will appear higher in Similar Ads for 3 days
+              </div>
+              <div class="divv-41">Analytics for as long as the ad is live</div>
+            </div>
+            <div class="divv-42">From Rs 600</div>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/1706ae8f17adcaf7f3403231ebb015c8462dfa93fb8f06f41d3f691158239c4b?"
+              class="img-9"
+            />
+          </div>
+          
+          
+          <div class="divv-43"></div>
+          <div class="divv-44">
+            <div class="divv-45">
+              <img
+                loading="lazy"
+                src="../public/img/payment/images/alert.png"
+                class="img-10"
+              />
+              URGENT
+            </div>
+            <div class="divv-47">Urgent</div>
+          </div>
+          <div class="divv-48">
+            <div class="divv-49">
+              Stand out from the rest by showing a bright red marker on the ad!
+            </div>
+            <div class="divv-50">
+              <div class="divv-51">From Rs 700</div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/1706ae8f17adcaf7f3403231ebb015c8462dfa93fb8f06f41d3f691158239c4b?"
+                class="img-11"
+              />
+            </div>
+          </div>
+          <div class="divv-52"></div>
+         
+         
+          <div class="divv-53">
+            <img
+              loading="lazy"
+              srcset="../public/img/payment/images/spotlight.png"
+              class="img-12"
+            />
+            <div class="divv-54">Spotlight</div>
+          </div>
+        </div>
+        <div class="divv-55">
 
-  <script>
-        function redirectToWebsite() {
-            // Replace "https://example.com" with the actual URL you want to link to
-            window.location.href = "http://localhost/ecotrade/PaymentPortal/index";
-        }
-    </script>
-
+        <div class="divv-250">
+        Boost sales by showing your ad in this premium slot.
+            </div>
+          <div class="divv-56">From Rs 2,400</div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/1706ae8f17adcaf7f3403231ebb015c8462dfa93fb8f06f41d3f691158239c4b?"
+            class="img-13"
+          />
+        </div>
+      </div>
 </div>
-
-
-</div>
+    </div>
+    <div class="divv-57"></div>
+    <div class="divv-58">
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/88abaee7ff6e6e5cedf3bebf405f25fee6efd9189ff99f7521dd92b6f4a69adc?"
+        class="img-14"
+      />
+      <div class="divv-59">Your ad is under review!</div>
+      <div class="divv-60">
+        Please note that it can take up to 4 hours for your ad to be published.
+        Keep track of your ad through
+      </div>
+      <div class="divv-61">
+        We will contact you shortly if payment is required to publish your ad.
+      </div>
+      <div class="divv-62">
+      <img
+        loading="lazy"
+        src="../public/img/payment/images/u-4.jpg"
+        class="img-200"
+      />
+        <div class="divv-63">
+          
+          <div class="divv-64">Huawei Y7a (Used)</div>
+          <div class="divv-65">Colombo 3, Mobile Phones</div>
+          <div class="divv-66">Rs 25,000</div>
+        </div>
+      </div>
+      <div class="divv-67">
+        <div class="divv-68">
+          <div class="divv-69">
+            <div class="divv-70">Payment summary</div>
+            <div class="divv-71">Bump Up</div>
+          </div>
+          <div class="divv-72">Rs 2,000</div>
+        </div>
+        <div class="divv-73"></div>
+        <div class="divv-74">
+          <div class="divv-75">Total amount</div>
+          <div class="divv-76">Rs 2,000</div>
+        </div>
+      </div>
+      <div class="divv-77">Select a payment method</div>
+      <div class="divv-78">
+        <div class="divv-79"></div>
+        <div class="divv-80">Pay online</div>
+        <img
+        loading="lazy"
+        src="../public/img/payment/images/Untitled design (1).png"
+        class="img-400"
+      />
+      </div>
+      <div class="divv-81">
+        <div class="divv-82"></div>
+        <div class="divv-83">Pay by voucher</div>
+        <img
+        loading="lazy"
+        src="../public/img/payment/images/voucher.png"
+        class="img-500"
+      />
+      </div>
+      <div class="divv-84">Continue</div>
+    </div>
+  </div>
+  </div>
 <!-- <?php require APPROOT . '/views/inc/components/footer.php';?> -->
 </body>
 </html>
