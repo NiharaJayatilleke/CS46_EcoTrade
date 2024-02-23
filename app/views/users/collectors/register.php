4321efd2e1984abf849e8cd44a6ce9c02e2ecca6
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-02-23 15:14:50 +0530

    registered colloector media

--- app/views/users/collectors/register.php
+++ app/views/users/collectors/register.php
@@ -229,23 +229,3 @@
 </div>
 
 <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/collectors/register.js"></script>
-
-<!-- <script>
-    const form = document.querySelector("form"),
-        nextBtn = form.querySelector(".nextBtn"),
-        backBtn = form.querySelector(".backBtn"),
-        allInput = form.querySelectorAll(".first input");
-
-    nextBtn.addEventListener("click", () => {
-        allInput.forEach(input => {
-            if (input.value !== "") {
-                form.classList.add('secActive');
-            } else {
-                form.classList.remove('secActive');
-            }
-        });
-    });
-
-    backBtn.addEventListener("click", () => form.classList.remove('secActive'));
-
-</script> -->
