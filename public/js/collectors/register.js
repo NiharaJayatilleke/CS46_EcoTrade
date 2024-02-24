57a27cc44584cdfbbb5f96c9c76bb57dc70b313c
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-02-24 16:24:55 +0530

    moderator index paved11

--- public/js/collectors/register.js
+++ public/js/collectors/register.js
@@ -25,12 +25,13 @@ nextBtn.addEventListener("click", (event) => {
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
 
 
+
 // Function to update options of subsequent select fields based on the selection made
-function updateOptions(selectedValues, selectFields) {
+function updateOptions(selectedValues, selectFields, currentSelect) {
     for (let i = 0; i < selectFields.length; i++) {
         const options = selectFields[i].getElementsByTagName('option');
         for (let j = 0; j < options.length; j++) {
-            if (selectedValues.includes(options[j].value)) {
+            if (selectedValues.includes(options[j].value) && selectFields[i] !== currentSelect) {
                 options[j].disabled = true; // Disable the selected option
             } else {
                 options[j].disabled = false; // Enable other options
@@ -49,12 +50,58 @@ var selectedValues = [];
 selectFields.forEach(selectField => {
     selectField.addEventListener('change', function() {
         const selectedValue = this.value;
-
+        
+        // Remove the previously selected value of this select field from the array of selected values
+        const previousValue = this.getAttribute('data-previous-value');
+        if (previousValue) {
+            const index = selectedValues.indexOf(previousValue);
+            if (index > -1) {
+                selectedValues.splice(index, 1);
+            }
+        }
+        
         // Add the selected value to the array of selected values if it was not previously selected
         if (!selectedValues.includes(selectedValue)) {
             selectedValues.push(selectedValue);
         }
-
-        updateOptions(selectedValues, selectFields);
+        
+        // Update the data-previous-value attribute with the new selected value
+        this.setAttribute('data-previous-value', selectedValue);
+        
+        updateOptions(selectedValues, selectFields, this);
     });
 });
+
+// // Function to update options of subsequent select fields based on the selection made
+// function updateOptions(selectedValues, selectFields) {
+//     for (let i = 0; i < selectFields.length; i++) {
+//         const options = selectFields[i].getElementsByTagName('option');
+//         for (let j = 0; j < options.length; j++) {
+//             if (selectedValues.includes(options[j].value)) {
+//                 options[j].disabled = true; // Disable the selected option
+//             } else {
+//                 options[j].disabled = false; // Enable other options
+//             }
+//         }
+//     }
+// }
+
+// // Get all select fields
+// const selectFields = document.querySelectorAll('.district-select');
+
+// // Store all selected values
+// var selectedValues = [];
+
+// // Add change event listener to each select field
+// selectFields.forEach(selectField => {
+//     selectField.addEventListener('change', function() {
+//         const selectedValue = this.value;
+
+//         // Add the selected value to the array of selected values if it was not previously selected
+//         if (!selectedValues.includes(selectedValue)) {
+//             selectedValues.push(selectedValue);
+//         }
+
+//         updateOptions(selectedValues, selectFields);
+//     });
+// });
