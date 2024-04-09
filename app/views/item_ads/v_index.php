e3c683d23c8f34104d33e753873f919b40622974
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2024-04-09 15:54:30 +0530

    issue with ads appearing more than once

--- app/views/item_ads/v_index.php
+++ app/views/item_ads/v_index.php
@@ -86,7 +86,11 @@
             
         });
 
-        foreach($data['ads'] as $ad): ?>
+        $uniqueIds = array_unique(array_column($data['ads'], 'p_id'));
+        $uniqueAds = array_intersect_key($data['ads'], $uniqueIds);
+
+        foreach($uniqueAds as $ad): ?>
+        // foreach($data['ads'] as $ad): >
             <a class = "ad-show-link" href="<?php echo URLROOT;?>/ItemAds/show/<?php echo $ad->ad_id?>">
                 <div class = "ad-index-container"
                 data-price="<?php echo $ad->item_price ?>"
