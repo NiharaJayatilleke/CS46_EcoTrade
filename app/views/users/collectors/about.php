eefe8a1f874dcb888510b1e4b04c4912ae2b41ba
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-04-16 17:33:41 +0530

    collectorabout2

--- app/views/users/collectors/about.php
+++ app/views/users/collectors/about.php
@@ -27,40 +27,40 @@
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-heading">
-                        <h2>Choose <em>Program</em></h2>
-                        <img src="assets/images/line-dec.png" alt="waves">
-                        <p>Training Studio is free CSS template for gyms and fitness centers. You are allowed to use this layout for your business website.</p>
+                        <h2>Beneficts of being a <em>Collector</em></h2>
+                        <img src="../public/img/collectors/images/line-dec.png" alt="waves">
+                        <p>Join us in collecting recyclable items and earn extra income while making a positive impact on the environment! </p>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <ul class="features-items">
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="First One">
+                                <img src="../public/img/collectors/images/11.jpg" alt="First One">
                             </div>
                             <div class="right-content">
-                                <h4>Basic Fitness</h4>
-                                <p>Please do not re-distribute this template ZIP file on any template collection website. This is not allowed.</p>
+                                <h4>Extra Income Stream</h4>
+                                <p>Collecting recyclables offers individuals an additional source of income.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="second one">
+                                <img src="../public/img/collectors/images/11.jpg" alt="second one">
                             </div>
                             <div class="right-content">
-                                <h4>New Gym Training</h4>
+                                <h4>Low Startup Costs</h4>
                                 <p>If you wish to support TemplateMo website via PayPal, please feel free to contact us. We appreciate it a lot.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="third gym training">
+                                <img src="../public/img/collectors/images/11.jpg" alt="third gym training">
                             </div>
                             <div class="right-content">
-                                <h4>Basic Muscle Course</h4>
-                                <p>Credit goes to <a rel="nofollow" href="https://www.pexels.com" target="_blank">Pexels website</a> for images and video background used in this HTML template.</p>
+                                <h4>Scalable Income Potential</h4>
+                                <p>There's a steady demand for recyclable materials, ensuring a reliable income stream.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
@@ -70,31 +70,31 @@
                     <ul class="features-items">
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="fourth muscle">
+                                <img src="../public/img/collectors/images/11.jpg" alt="fourth muscle">
                             </div>
                             <div class="right-content">
-                                <h4>Advanced Muscle Course</h4>
-                                <p>You may want to browse through <a rel="nofollow" href="https://templatemo.com/tag/digital-marketing" target="_parent">Digital Marketing</a> or <a href="https://templatemo.com/tag/corporate">Corporate</a> HTML CSS templates on our website.</p>
+                                <h4>Consistent Demand</h4>
+                                <p>There's a steady demand for recyclable materials, ensuring a reliable income stream.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="training fifth">
+                                <img src="../public/img/collectors/images/11.jpg" alt="training fifth">
                             </div>
                             <div class="right-content">
-                                <h4>Yoga Training</h4>
-                                <p>This template is built on Bootstrap v4.3.1 framework. It is easy to adapt the columns and sections.</p>
+                                <h4>Incentive Programs</h4>
+                                <p>Some platforms offer bonuses and incentive programs to boost collector earnings.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
                         <li class="feature-item">
                             <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="gym training">
+                                <img src="../public/img/collectors/images/11.jpg" alt="gym training">
                             </div>
                             <div class="right-content">
-                                <h4>Body Building Course</h4>
-                                <p>Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.</p>
+                                <h4>Market Value Opportunities</h4>
+                                <p>The market value of recyclable materials provides opportunities for profit.</p>
                                 <a href="#" class="text-button">Discover More</a>
                             </div>
                         </li>
@@ -106,8 +106,9 @@
     <!-- ***** Features Item End ***** -->
 
     <!-- ***** Call to Action Start ***** -->
-    <section class="section" id="call-to-action">
-        <div class="container">
+    <section class="section" id="call-to-action" style="background-image: url('../public/img/collectors/images/cta-bg.jpg');">
+    <div class="overlay23"></div>
+        <div class="container455">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="cta-content">
@@ -124,7 +125,7 @@
     <!-- ***** Call to Action End ***** -->
 
     <!-- ***** Our Classes Start ***** -->
-    <section class="section" id="our-classes">
+    <!-- <section class="section" id="our-classes">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
@@ -183,9 +184,9 @@
               </div>
             </div>
         </div>
-    </section>
+    </section> -->
     <!-- ***** Our Classes End ***** -->
-    
+<!--     
     <section class="section" id="schedule">
         <div class="container">
             <div class="row">
@@ -249,10 +250,10 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 
     <!-- ***** Testimonials Starts ***** -->
-    <section class="section" id="trainers">
+    <!-- <section class="section" id="trainers">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
@@ -320,11 +321,11 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
     <!-- ***** Testimonials Ends ***** -->
     
     <!-- ***** Contact Us Area Starts ***** -->
-    <section class="section" id="contact-us">
+    <!-- <section class="section" id="contact-us">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-xs-12">
@@ -368,5 +369,5 @@
             </div>
         </div>
     </section>
-    <!-- ***** Contact Us Area Ends ***** -->
-    
+
+     -->
