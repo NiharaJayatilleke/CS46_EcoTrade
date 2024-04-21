ea6fc5ffe280e0ea29172a8cb4ae64b6f7a4ef47
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-04-21 22:14:56 +0530

    admin settings

---
+++ app/views/pages/v_aboutus.php
@@ -0,0 +1,105 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+
+<body class="b110">
+
+       
+ 
+    <section class="hero gridSection">
+        <div class="sectionDesc">
+            <h1 class="headline">
+                Recycle, Earn <span class="cryptoText">ECOTRADE</span>.
+            </h1>
+            <p class="sub-headline">
+                
+"EcoTrade is more than a platform; it's a movement. We unite buyers and collectors to transform waste into valuable resources. By connecting people with a shared vision of sustainability, we redefine recycling and contribute to a greener world. Join us in shaping a future where every eco-conscious action makes a difference. Together, let's create a sustainable tomorrow."
+            </p>
+            <div class="btnContainer">
+                <button class="btn btn110">Explore More</button>
+                
+            </div>
+        </div>
+        <div class="sectionPic bouncepic" id="sectionPic">
+            <img src="./public/img/about/wepik-export-20240421085733kCr4.png" alt="">
+        </div>
+    </section>
+
+    <!-- Carousel -->
+    <section>
+        <div class="carouselContainer">
+            <div class="eachCarousel eachCarouselBorder">
+                <img src="./public/img/about/recycling.png" alt="">
+                <article class="carouselDesc">
+                    <h1 class="carouselTitle">Recycle Selling</h1>
+                    <p class="carouselPara">
+                        Recycle selling on EcoTrade links recyclers with buyers, including recycling centers and collectors, fostering sustainability and waste reduction.</p>
+              
+                    <button class="btn carouselBtn">Explore </button>
+                </article>
+            </div>
+    
+            <div class="eachCarousel">
+                <img src="./public/img/about/paper-bag.png" alt="">
+                <article class="carouselDesc">
+                    <h1 class="carouselTitle">Recycle Buying</h1>
+                    <p class="carouselPara">
+                        Recycle buying on EcoTrade connects buyers with recyclers, including recycling centers and collectors, promoting sustainability and reducing waste.</p>
+                  
+                    <button class="btn carouselBtn">Explore </button>
+                </article>
+            </div>
+    
+            <div class="eachCarousel">
+                <img src="./public/img/about/grocery-store.png" alt="">
+                <article class="carouselDesc">
+                    <h1 class="carouselTitle">Eco Market</h1>
+                    <p class="carouselPara">
+                        Eco market on EcoTrade facilitates the buying and selling of secondhand items, fostering sustainability and reducing waste through a shared e-commerce platform.</p>
+                    <button class="btn carouselBtn1">Explore </button>
+                </article>
+            </div>
+    
+        <div class="carouselIndicator">
+            <div class="indicator activeIndicator" onclick="slideCarousel(0)"></div>
+            <div class="indicator" onclick="slideCarousel(1)"></div>
+            <div class="indicator" onclick="slideCarousel(2)"></div>
+            <div class="indicator" onclick="slideCarousel(3)"></div>
+            <div class="indicator" onclick="slideCarousel(4)"></div>
+            <div class="indicator" onclick="slideCarousel(5)"></div>
+        </div>
+    </section>
+
+    <!-- Processes -->
+    <section class="gridSection">
+        <div class="sectionDesc processessDesc">
+            <h1 class="sectionHeader">Main Goal</h1>
+            <p class="sectionPara">We do not charge any fees and we do not require 
+              any registration. You keep your privacy and your 
+              coins.
+            </p>
+            <div class="eachProcesses">
+                <img src="./public/img/about/handshake-icon-white-line.svg" alt="handshake">
+                <div class="eachprocessesPara">
+                    <h1 class="processTitle">Recycle</h1>
+                    <p>
+                        Turn waste into valuable resources. Make recycling part of your everyday routine to help the planet.                    </p>
+                </div>
+            </div>
+            <div class="eachProcesses">
+                <img src="./public/img/about/cart-icon-white-line.svg" alt="handshake">
+                <div class="eachprocessesPara">
+                    <h1 class="processTitle">Earn</h1>
+                    <p>
+                        Turn recycling into an opportunity to earn. Collect, recycle, and get rewarded for your eco-friendly contributions.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="sectionPic bouncepic processesPic" id="sectionPic">
+            <img src="./public/img/about/wepik-export-20240421093650W1dh.png" alt="">
+        </div>
+    </section>
+    <script src="./script.js"></script>
+
+</body>
---
+++ app/views/users/recyclecenters/about.php
@@ -0,0 +1,140 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!-- Top NAVIGATION -->
+<?php require APPROOT . '/views/inc/components/topnavbar.php';?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/collectors/about.css">
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner" id="top">
+        <video autoplay muted loop id="bg-video">
+            <source src="../public/img/Recyclehome/image/3195440-uhd_3840_2160_25fps.mp4" type="video/mp4" />
+        </video>
+
+        <div class="video-overlay header-text">
+            <div class="caption">
+                <h6>Recycle, Earn</h6>
+                <h2>become <em>recycle center</em></h2>
+                <div class="main-button scroll-to-section">
+                    <a href="<?php echo URLROOT ?>/Collectors/register">register now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** Features Item Start ***** -->
+    <section class="section" id="features">
+        <div class="container">
+            <div class="row1">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Beneficts of being a <em>Recycle Center</em></h2>
+                        <img src="../public/img/collectors/images/line-dec.png" alt="waves">
+                        <p>Join us in collecting recyclable items and earn extra income while making a positive impact on the environment! </p>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="First One">
+                            </div>
+                            <div class="right-content">
+                                <h4>Extra Income Stream</h4>
+                                <p>Collecting recyclables offers individuals an additional source of income.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="second one">
+                            </div>
+                            <div class="right-content">
+                                <h4>Low Startup Costs</h4>
+                                <p>If you wish to support TemplateMo website via PayPal, please feel free to contact us. We appreciate it a lot.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="third gym training">
+                            </div>
+                            <div class="right-content">
+                                <h4>Scalable Income Potential</h4>
+                                <p>There's a steady demand for recyclable materials, ensuring a reliable income stream.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="fourth muscle">
+                            </div>
+                            <div class="right-content">
+                                <h4>Consistent Demand</h4>
+                                <p>There's a steady demand for recyclable materials, ensuring a reliable income stream.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="training fifth">
+                            </div>
+                            <div class="right-content">
+                                <h4>Incentive Programs</h4>
+                                <p>Some platforms offer bonuses and incentive programs to boost collector earnings.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="../public/img/collectors/images/11.jpg" alt="gym training">
+                            </div>
+                            <div class="right-content">
+                                <h4>Market Value Opportunities</h4>
+                                <p>The market value of recyclable materials provides opportunities for profit.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Item End ***** -->
+
+    <!-- ***** Call to Action Start ***** -->
+    <!-- <section class="section" id="call-to-action" style="background-image: url('../public/img/collectors/images/cta-bg.jpg');">
+    <div class="overlay23">
+        <div class="container455">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="cta-content">
+                        <h2>Don’t <em>think</em>, begin <em>today</em>!</h2>
+                        <p>Ut consectetur, metus sit amet aliquet placerat, enim est ultricies ligula, sit amet dapibus odio augue eget libero. Morbi tempus mauris a nisi luctus imperdiet.</p>
+                        <div class="main-button scroll-to-section">
+                            <a href="#our-classes">Become a member</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+
+        <section id="workFlow">
+        <div class="overlay43">
+        <h2 class="heading"> Grow Up Your Workflow Speed. </h2>
+        <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi sint. <br>Ipsam molestias
+            nemovel laboriosam consequatur, perferendis<br> minima soluta? Natus necessitatibus autem suscipit!</p>
+        <div class="num-container">
+            <div class="num-item1"><span>27,882 <br>Customers</span></div>
+            <div class="num-item1"><span>90% <br>Action Plans</span></div>
+            <div class="num-item1"><span>70,592 <br>Downloads</span></div>
+        </div>
+        </div>
+    </section>
