4ea0305f622ecb2220bdd49dc7fcf3a318f1c9fe
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-04-21 16:34:58 +0530

    aboutuscomplete

--- app/controllers/Aboutus.php
+++ app/controllers/Aboutus.php
@@ -1,12 +1,12 @@
 <?php
-    class Help extends Controller{
+    class Aboutus extends Controller{
         public function __construct(){
             $this->pagesModel =$this->model('M_Pages');
         }
 
         public function index(){
             $data = [];
-            $this->view('pages/v_help',$data);
+            $this->view('pages/v_aboutus',$data);
 
         }
     }
