faca9184d176e06af1e24b1dd9c81a88b88c7a17
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-04-22 16:25:10 +0530

    recenteradview2

--- app/controllers/Recenters.php
+++ app/controllers/Recenters.php
@@ -18,6 +18,39 @@
 
     }
 
+     
+    // public function recenteradShow($adId){
+    //     $ad = $this->recentersModel->getAdById($adId);
+    //     // $seller = $this->recycleItemAdsModel->getSellerByAdId($adId);
+        
+    //     $data = [
+    //         'ad' => $ad,
+    //         // 'seller' => $seller,
+    //     ];
+
+    //     $this->view('recenters/v_show',$data);
+    // }
+
+    // public function edit(){
+
+    // }
+
+    // public function delete(){
+        // $ad = $this->RecycleItemAdsModel->getAdById($adId);
+        // $oldImage = PUBROOT.'/img/items/'.$ad->item_image;
+        // deleteImage($oldImage);
+
+//         if($this->RecycleItemAdsModel->delete($adId)){
+//             flash('post_msg', 'Your ad has been deleted successfully!');
+//             redirect('ItemAds/index');
+//         }
+//         else{
+//             die('Something went wrong');
+//         }
+//     }
+
+// }
+
 
     public function recenters(){
         // show("Recenters");
