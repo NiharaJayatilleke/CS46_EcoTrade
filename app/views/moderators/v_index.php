da27a80674fcdaab47e489cc17f3c3c51cad0afc
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-04-23 12:31:24 +0530

    corrected divs

--- app/views/moderators/v_index.php
+++ app/views/moderators/v_index.php
@@ -344,7 +344,6 @@
 
                                 </tbody>
                             </table>
-                        </div>
                     </div>
                 </div>
             </div>
