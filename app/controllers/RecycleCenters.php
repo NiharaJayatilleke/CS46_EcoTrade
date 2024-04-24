7e32c6e5779fd75ec6a672e7f6683e5cee94e901
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-04-24 17:00:59 +0530

    center register okay with alerts

--- app/controllers/RecycleCenters.php
+++ app/controllers/RecycleCenters.php
@@ -144,7 +144,7 @@
                 if(empty($data['nic_err']) && empty($data['gender_err']) && empty($data['address_err']) && empty($data['com_name_err']) && empty($data['com_email_err']) && empty($data['com_address_err']) && empty($data['telephone_err']) && empty($data['company_type_err']) && empty($data['reg_number_err']) && empty($data['vehicle_type_err']) && empty($data['vehicle_reg_err']) && empty($data['model_err']) && empty($data['other_vehicle_err']) &&  empty($data['color_err']) && empty($data['districts_err'])){
                     // Register collector
                     
-                    if($this->recyclecenterModel->register($data)){
+                    if($this->recentersModel->register($data)){
                         // Create a flash message
                         flash('reg_flash', 'You are successfully registered as a center!');
 
--- public/js/center/register.js
+++ public/js/center/register.js
@@ -35,85 +35,85 @@ backBtn.addEventListener("click", (event) => {
 });
 
 
-// function confirmTerms(event) {
-//     event.preventDefault();
+function confirmTerms(event) {
+    event.preventDefault();
 
-//     var form = event.target.form;
+    var form = event.target.form;
 
-//     // Check if the form is valid
-//     if (!form.checkValidity()) {
-//         // Not all required fields are filled, show an error message
-//         Swal.fire({
-//             title: 'Error!',
-//             text: 'There are some more fields to be filled. Please fill them and try again.',
-//             icon: 'error'
-//         });
-//         return;
-//     }
+    // Check if the form is valid
+    if (!form.checkValidity()) {
+        // Not all required fields are filled, show an error message
+        Swal.fire({
+            title: 'Error!',
+            text: 'There are some more fields to be filled. Please fill them and try again.',
+            icon: 'error'
+        });
+        return;
+    }
 
-//     // Check if at least one checkbox is checked
-//     var checkboxes = form['categories[]'];
-//     var oneCheckboxChecked = Array.prototype.some.call(checkboxes, function(checkbox) {
-//         return checkbox.checked;
-//     });
+    // Check if at least one checkbox is checked
+    var checkboxes = form['categories[]'];
+    var oneCheckboxChecked = Array.prototype.some.call(checkboxes, function(checkbox) {
+        return checkbox.checked;
+    });
 
-//     if (!oneCheckboxChecked) {
-//         // No checkbox is checked, show an error message
-//         Swal.fire({
-//             title: 'Error!',
-//             text: 'Please select at least one category.',
-//             icon: 'error'
-//         });
-//         return;
-//     }
+    if (!oneCheckboxChecked) {
+        // No checkbox is checked, show an error message
+        Swal.fire({
+            title: 'Error!',
+            text: 'Please select at least one category.',
+            icon: 'error'
+        });
+        return;
+    }
 
-//     // If the form is valid, show the confirmation dialog
-//     var formData = new FormData(form);
+    // If the form is valid, show the confirmation dialog
+    var formData = new FormData(form);
 
-//     Swal.fire({
-//     title: 'Are you sure you want to register as a Recycle Center on Eco Trade?',
-//     html: `
-//         <p>Once you submit your registration, your information will be processed, and you'll be on your way to contributing to a greener future. If you're ready to take this step, go ahead and hit submit!</p>
-//         <input type="checkbox" id="terms" name="terms">
-//         <label for="terms">I agree to the Terms and Conditions</label>
-//     `,
-//     icon: 'question',
-//     showCancelButton: true,
-//     confirmButtonColor: '#3085d6',
-//     cancelButtonColor: '#d33',
-//     confirmButtonText: 'Submit',
-//     preConfirm: () => {
-//         const terms = Swal.getPopup().querySelector('#terms').checked
-//         if (!terms) {
-//             Swal.showValidationMessage(`Please agree to the Terms and Conditions`)
-//         }
-//     },
-//     didOpen: () => {
-//         document.querySelector('.swal2-content').style.fontSize = '0.9em'; // Adjust the font size to your liking
-//     }
-// }).then((result) => {
-//     if (result.isConfirmed) {
-//         // Submit the form using AJAX
-//         var xhr = new XMLHttpRequest();
-//         xhr.open(form.method, form.action);
-//         xhr.onreadystatechange = function () {
-//             if (xhr.readyState === 4 && xhr.status === 200) {
-//                 Swal.fire({
-//                     title: 'Success!',
-//                     text: 'You are successfully registered as a Recycle Center in Eco Trade.',
-//                     icon: 'success'
-//                 }).then(() => {
-//                     // Redirect to the login page with a message
-//                     var message = encodeURIComponent('Please login again to continue as a Recycle Center');
-//                     window.location.href = '/ecotrade/Users/login?message=' + message;
-//                 });
-//             } else if (xhr.readyState === 4) {
-//                 // Handle the error
-//                 console.error('Form submission failed:', xhr.status, xhr.statusText);
-//             }
-//         };
-//         xhr.send(formData);
-//     }
-// })
-// }
+    Swal.fire({
+    title: 'Are you sure you want to register as a Recycle Center on Eco Trade?',
+    html: `
+        <p>Once you submit your registration, your information will be processed, and you'll be on your way to contributing to a greener future. If you're ready to take this step, go ahead and hit submit!</p>
+        <input type="checkbox" id="terms" name="terms">
+        <label for="terms">I agree to the Terms and Conditions</label>
+    `,
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Submit',
+    preConfirm: () => {
+        const terms = Swal.getPopup().querySelector('#terms').checked
+        if (!terms) {
+            Swal.showValidationMessage(`Please agree to the Terms and Conditions`)
+        }
+    },
+    didOpen: () => {
+        document.querySelector('.swal2-content').style.fontSize = '0.9em'; // Adjust the font size to your liking
+    }
+}).then((result) => {
+    if (result.isConfirmed) {
+        // Submit the form using AJAX
+        var xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'You are successfully registered as a Recycle Center in Eco Trade.',
+                    icon: 'success'
+                }).then(() => {
+                    // Redirect to the login page with a message
+                    var message = encodeURIComponent('Please login again to continue as a Recycle Center');
+                    window.location.href = '/ecotrade/Users/login?message=' + message;
+                });
+            } else if (xhr.readyState === 4) {
+                // Handle the error
+                console.error('Form submission failed:', xhr.status, xhr.statusText);
+            }
+        };
+        xhr.send(formData);
+    }
+})
+}
 
