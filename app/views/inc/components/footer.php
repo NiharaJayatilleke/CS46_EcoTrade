63d42be9646ed63c369878eb4d88b1e05967aaa5
rusiruwi <149309761+rusiruwi@users.noreply.github.com>
2024-04-26 12:22:26 +0530

    footer

--- app/views/inc/components/footer.php
+++ app/views/inc/components/footer.php
@@ -1,8 +1,8 @@
-<footer class="footer">
+<!-- <footer class="footer">
 	<div class="footer_container">
 		<div class="rowfooter">
 			<div class="footer-col">
-				<img class="img" src="<?php echo URLROOT; ?>/img/signup/ecotrade.png" >
+				<img class="img" src="<//?php echo URLROOT; ?>/img/signup/ecotrade.png" >
 				<h3>EcoTrade</h3>
 			</div>
 			<div class="footer-col">
@@ -32,4 +32,38 @@
 </footer>
 
 </body>
-</html>
+</html> -->
+
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
+
+<body class= "footer69">
+<footer>
+<div class="footer">
+<div class="row">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-instagram"></i></a>
+<a href="#"><i class="fa fa-youtube"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+</div>
+
+<div class="row">
+<ul>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Log In</a></li>
+<li><a href="#">SecondHand Market</a></li>
+<li><a href="#">Recycle Market</a></li>
+<li><a href="#">Privacy Policy</a></li>
+</ul>
+</div>
+
+<div class="row">
+  EcoTrade Copyright © 2021 EcoTrade - All rights reserved    
+</div>
+</div>
+</footer>
