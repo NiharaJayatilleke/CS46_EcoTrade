f04f0a6a13e010fe210133aed4a2f0ef54c9a3ad
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2024-04-28 17:33:08 +0530

    recycle item images and expiry model

--- app/models/M_Recycle_Item_Ads.php
+++ app/models/M_Recycle_Item_Ads.php
@@ -6,17 +6,32 @@
             $this->db = new Database();
         }
 
-        public function register($data) {
-            $this->db->query('INSERT INTO Recycle_Item_Ads(seller_id,item_name,item_category,item_desc,item_image,item_location) VALUES(:seller_id, :item_name, :item_category, :item_desc, :item_image, :item_location)'); 
+        public function re_create($data) {
+            $this->db->query('INSERT INTO Recycle_Item_Ads(seller_id,item_name,item_category,item_desc,item_image,item_location,item_expiry,status) VALUES(:seller_id, :item_name, :item_category, :item_desc, :item_image, :item_location, :item_expiry, :status)'); 
             $this->db->bind(':seller_id',$_SESSION['user_id']);         
             $this->db->bind(':item_name',$data['item_name']);
             $this->db->bind(':item_category',$data['item_category']);
             $this->db->bind(':item_desc',$data['item_desc']);  
             $this->db->bind(':item_image',$data['item_img_name']);
             $this->db->bind(':item_location',$data['item_location']);
+            $this->db->bind(':item_expiry',$data['item_expiry']);
+            $this->db->bind(':status',"active");
             
             if($this->db->execute()){
-                return true;
+                // return true;
+                $item_id = $this->db->lastInsertId();
+
+                for($i = 1; $i < count($data['item_img_name']); $i++) {
+                    $this->db->query('INSERT INTO Recycle_Ad_Images(item_id, image_name) VALUES(:item_id, :image_name)');
+                    $this->db->bind(':item_id', $item_id);
+                    $this->db->bind(':image_name', $data['item_img_name'][$i]);
+                    // $this->db->execute();
+                    if(!$this->db->execute()){
+                        print_r($this->db->errorInfo());
+                    }
+                }
+        
+                return $item_id;
             }
             else{
                 return false;
