5ef37c225d95593ba2a3ec4442f28e7bd08eb23f
Mr. I.N.WEERASEKERA <2021cs211@stu.ucsc.cmb.ac.lk>
2024-04-30 04:45:49 +0530

    remove colour and added responsiveness 5

--- public/js/moderators/reportads.js
+++ public/js/moderators/reportads.js
@@ -12,6 +12,8 @@ function confirmDeleteReportedad(adId) {
         text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!',
         cancelButtonText: 'No, cancel!',
         reverseButtons: true
